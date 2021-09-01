ae34c8334818380f41747ac42d56351c81ba4e0b
Yamid OM <yaormu@gmail.com>
2021-09-01 17:40:28 -0500

    login google

--- app/src/main/java/com/namebussine/myapplicationtenderos/AuthActivity.kt
+++ app/src/main/java/com/namebussine/myapplicationtenderos/AuthActivity.kt
@@ -6,11 +6,21 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuth.*
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         //Splash
@@ -19,12 +29,6 @@ class AuthActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-
-        //Analytics Events
-        //val analytics = FirebaseAnalytics.getInstance(this)
-        //val bundle = Bundle()
-        //bundle.putString("message", "Integración de Firebase complet")
-        //analytics.logEvent("InitScreen", bundle)
 
         //Setup
         setup()
@@ -78,6 +82,21 @@ class AuthActivity : AppCompatActivity() {
                         }
                 }
         }
+
+        googleButton.setOnClickListener {
+            //Configuration
+            val googleConfig = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConfig)
+
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+
+        }
     }
 
     private fun showAlert() {
@@ -98,6 +117,34 @@ class AuthActivity : AppCompatActivity() {
             putExtra("provider", provider.name)
         }
         startActivity(homeIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GOOGLE_SIGN_IN) {
+
+            val task:Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account:GoogleSignInAccount = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+
+                    val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(account.email ?: "", ProviderType.GOOGLE)
+                        }else {
+                            showAlert()
+                        }
+                    }
+                }
+            } catch (e: ApiException) {
+                showAlert()
+            }
+        }
     }
 
 
